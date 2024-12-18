e433469c5b555527f216a64ad07574d5d2d839b7
ImSoBad123 <138328968+ImSoBad123@users.noreply.github.com>
2024-12-18 22:23:51 +0700

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -11,22 +11,20 @@ while true do
         if button then
             for _, connection in ipairs(getconnections(button.MouseButton1Click)) do
                 if connection.Function then
-                    connection.Function() -- Manually trigger the button function
+                    connection.Function()
                 end
             end
         end
     else
-        break -- Exit loop if DeviceSelect is gone
+        break
     end
-    wait() -- Avoid spamming too fast
+    wait()
 end
 
-
-D3RenderingDisabled = true
+D3RenderingDisabled = false
 ImproveFPSenabled = true
 CoinTypes = {"Coin"}
 CurrentCoinType = "Coin"
-PositionOfCoinType = 1
 AutofarmDelay = 2
 ResetWhenFullBag = true
 AutofarmIN = true
@@ -37,6 +35,28 @@ RunService = game:GetService("RunService")
 CoinCollectedEvent = game.ReplicatedStorage.Remotes.Gameplay.CoinCollected
 RoundStartEvent = game.ReplicatedStorage.Remotes.Gameplay.RoundStart
 RoundEndEvent = game.ReplicatedStorage.Remotes.Gameplay.RoundEndFade
+
+local function activateSpin(args, speaker)
+    local spinSpeed = tonumber(args[1]) or 20
+    local character = speaker.Character or speaker.CharacterAdded:Wait()
+    local rootPart = character:WaitForChild("HumanoidRootPart")
+
+    for _, v in pairs(rootPart:GetChildren()) do
+        if v.Name == "Spinning" then v:Destroy() end
+    end
+
+    local Spin = Instance.new("BodyAngularVelocity")
+    Spin.Name, Spin.Parent, Spin.MaxTorque, Spin.AngularVelocity = "Spinning", rootPart, Vector3.new(0, math.huge, 0), Vector3.new(0, spinSpeed, 0)
+end
+
+local function stopSpin(speaker)
+    local character = speaker.Character or speaker.CharacterAdded:Wait()
+    local rootPart = character:WaitForChild("HumanoidRootPart")
+
+    for _, v in pairs(rootPart:GetChildren()) do
+        if v.Name == "Spinning" then v:Destroy() end
+    end
+end
 
 function AntiAFK()
 	local GC = getconnections or get_signal_cons
@@ -101,7 +121,7 @@ safepart.Anchored = true
 safepart.Massless = true
 safepart.Transparency = 1
 safepart.Size = Vector3.new(2048, 0.5, 2048)
-safepart.CFrame = bringpose * CFrame.new(0, -0.9, 0)
+safepart.CFrame = bringpose * CFrame.new(0, -1.2, 0)
 safepart.Parent = workspace
 
 function returncoincontaier()
@@ -131,19 +151,19 @@ function PcallTP(Position)
 	end
 end
 
+
 spawn(function()
 	while true do
 		if AutofarmStarted and AutofarmIN and Player.Character and returncoincontaier() then
 			PcallTP(bringpose)
 			for _, v in pairs(returncoincontaier():GetChildren()) do
 				if v:GetAttribute("CoinID") == CurrentCoinType and v:FindFirstChild("TouchInterest") then
-					for i = 1,7 do
-						PcallTP(v.CFrame)
-						task.wait(0.03)
-					end
+                    activateSpin({15}, game.Players.LocalPlayer)
+					PcallTP(v.CFrame)
 					break
 				end
 			end
+            wait(0.2)
 			PcallTP(bringpose)
 		end
 		task.wait(AutofarmDelay)
@@ -184,7 +204,7 @@ Players.PlayerAdded:Connect(function(player1)
 					part:Destroy()
 				end
 				if part.Name == "Radio" then
-					part:Destroy()
+					part:DestAroy()
 				end
 			end
 		end
