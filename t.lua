abfd185e3a9867fdeadf31ae9aa23bdfae6d1d22
ImSoBad123 <138328968+ImSoBad123@users.noreply.github.com>
2025-01-29 22:01:13 +0700

    Update t.lua

--- t.lua
+++ t.lua
@@ -1,8 +1,44 @@
-repeat wait() until game:IsLoaded() and game.ReplicatedStorage and game.ReplicatedStorage:FindFirstChild("MultiboxFramework")
-task.wait(5)
-repeat wait() until require(game:GetService("ReplicatedStorage").MultiboxFramework).Loaded
+getgenv().username = "ayipky"
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game.ReplicatedStorage and game.ReplicatedStorage:FindFirstChild("MultiboxFramework") and game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("FrameworkElements"):WaitForChild("ScreenBarrier"):WaitForChild("LoadingTitle").Visible
+repeat task.wait() until game.Players and game.Players.LocalPlayer
+local framework = require(game:GetService("ReplicatedStorage").MultiboxFramework)
+repeat task.wait() until framework and framework.Loaded
+
 if game.PlaceId == 13775256536 then
-local args = {
+    local Players = game:GetService("Players")
+    getgenv().userId = Players:GetUserIdFromNameAsync(getgenv().username)
+    print("send gift")
+    local args = {
+        [1] = {
+            [1] = {
+                [1] = "\226\129\130l",
+                [2] = "GoldenGladiatorCrate",
+                [3] = "Buy1",
+                [4] = getgenv().userId
+            }
+        }
+    }
+
+    game:GetService("ReplicatedStorage"):WaitForChild("NetworkingContainer"):WaitForChild("DataRemote"):FireServer(unpack(args))
+    wait(1)
+    local args = {
+        [1] = {
+            [1] = {
+                [1] = "\226\129\130g",
+                [2] = "GoldenGladiatorCrate",
+                [3] = "Buy1",
+                [4] = getgenv().userId
+            }
+        }
+    }
+
+    game:GetService("ReplicatedStorage"):WaitForChild("NetworkingContainer"):WaitForChild("DataRemote"):FireServer(unpack(args))
+    local plr = game.Players.LocalPlayer
+    plr.Character.HumanoidRootPart.CFrame = workspace.Zones.Lobby.RankedPVPZone.CFrame
+    task.wait(1)    
+    print("Join PVP")
+    local args = {
         [1] = {
             [1] = {
                 [1] = game:GetService("ReplicatedStorage").IdentifiersContainer
@@ -11,8 +47,9 @@ local args = {
         }
     }
 
-game:GetService("ReplicatedStorage"):WaitForChild("NetworkingContainer"):WaitForChild("DataRemote"):FireServer(unpack(args))
+    game:GetService("ReplicatedStorage"):WaitForChild("NetworkingContainer"):WaitForChild("DataRemote"):FireServer(unpack(args))
 else
+print("Ingame")
 local VirtualUser = game:service "VirtualUser"
 game:service("Players").LocalPlayer.Idled:connect(
     function()
@@ -24,14 +61,7 @@ repeat wait() until game:GetService("Players").LocalPlayer.PlayerGui.FinishUI.Ma
 repeat wait() until game:GetService("Players").LocalPlayer.PlayerGui.Match.MatchInfo.AutoSkip.OnAndOff
 local VirtualInputManager = game:GetService('VirtualInputManager')
 local GuiService = game:GetService('GuiService')
-
-task.wait(0.3)
-GuiService.SelectedObject = game:GetService("Players").LocalPlayer.PlayerGui.Match.MatchInfo.AutoSkip.OnAndOff
-VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Return, false, game)
-VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Return, false, game)
-task.wait(0)
-GuiService.SelectedObject = nil
-while true do
+while wait() do
     task.wait(0.3)
 
     GuiService.SelectedObject = game:GetService("Players").LocalPlayer.PlayerGui.FinishUI.MatchFinish.MatchFinishFrame.EndOptions.ReturnToLobby.ButtonFrame.ReturnToLobbyButton
