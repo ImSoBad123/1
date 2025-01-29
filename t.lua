2b2adfa19f3c83de68362e06289a1ad57c692e26
ImSoBad123 <138328968+ImSoBad123@users.noreply.github.com>
2025-01-29 16:38:21 +0700

    Update t.lua

--- t.lua
+++ t.lua
@@ -1,27 +1,39 @@
+--[[getgenv().username = "ayipky"]]
 repeat task.wait() until game:IsLoaded()
 repeat task.wait() until game.ReplicatedStorage and game.ReplicatedStorage:FindFirstChild("MultiboxFramework") and game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("FrameworkElements"):WaitForChild("ScreenBarrier"):WaitForChild("LoadingTitle").Visible
-repeat task.wait() until game.Players and game.Players.LocalPlayer 
-
+repeat task.wait() until game.Players and game.Players.LocalPlayer
 local framework = require(game:GetService("ReplicatedStorage").MultiboxFramework)
-repeat wait() until framework and framework.Loaded
+repeat task.wait() until framework and framework.Loaded
 
 if game.PlaceId == 13775256536 then
-local Players = game:GetService("Players")
-getgenv().userId = Players:GetUserIdFromNameAsync(getgenv().username)
-
-local args = {
-    [1] = {
+    local Players = game:GetService("Players")
+    getgenv().userId = Players:GetUserIdFromNameAsync(getgenv().username)
+    print("send gift")
+    local args = {
         [1] = {
-            [1] = "\226\129\130l",
-            [2] = "GoldenGladiatorCrate",
-            [3] = "Buy1",
-            [4] = getgenv().userId
+            [1] = {
+                [1] = "\226\129\130l",
+                [2] = "GoldenGladiatorCrate",
+                [3] = "Buy1",
+                [4] = getgenv().userId
+            }
         }
     }
-}
 
-game:GetService("ReplicatedStorage"):WaitForChild("NetworkingContainer"):WaitForChild("DataRemote"):FireServer(unpack(args))
-    wait(1)    
+    game:GetService("ReplicatedStorage"):WaitForChild("NetworkingContainer"):WaitForChild("DataRemote"):FireServer(unpack(args))
+    local args = {
+        [1] = {
+            [1] = {
+                [1] = "\226\129\130g",
+                [2] = "GoldenGladiatorCrate",
+                [3] = "Buy1",
+                [4] = getgenv().userId
+            }
+        }
+    }
+
+    game:GetService("ReplicatedStorage"):WaitForChild("NetworkingContainer"):WaitForChild("DataRemote"):FireServer(unpack(args))
+    task.wait(1)    
     print("Join PVP")
     local args = {
         [1] = {
@@ -33,32 +45,30 @@ game:GetService("ReplicatedStorage"):WaitForChild("NetworkingContainer"):WaitFor
     }
 
     game:GetService("ReplicatedStorage"):WaitForChild("NetworkingContainer"):WaitForChild("DataRemote"):FireServer(unpack(args))
-
-
-
 else
     print("Ingame")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local TeleportService = game:GetService("TeleportService")
-local Players = game:GetService("Players")
+    local ReplicatedStorage = game:GetService("ReplicatedStorage")
+    local TeleportService = game:GetService("TeleportService")
+    local Players = game:GetService("Players")
 
-local MatchTimer = ReplicatedStorage:WaitForChild("MatchData"):WaitForChild("MatchTimer")
+    local MatchTimer = ReplicatedStorage:WaitForChild("MatchData"):WaitForChild("MatchTimer")
 
-local function checkTimer()
-    local initialValue = MatchTimer.Value
-    task.wait(5)
-    
-    -- Kiểm tra nếu MatchTimer.Value lớn hơn 0 mới thực hiện teleport
-    if MatchTimer.Value > 0 and MatchTimer.Value == initialValue then
-        for _, player in ipairs(Players:GetPlayers()) do
-            if player and player:IsA("Player") then
-                TeleportService:Teleport(13775256536, player)
+    local function checkTimer()
+        local initialValue = MatchTimer.Value
+        task.wait(5)
+
+        -- Kiểm tra nếu MatchTimer.Value lớn hơn 0 mới thực hiện teleport
+        if MatchTimer.Value > 0 and MatchTimer.Value == initialValue then
+            for _, player in ipairs(Players:GetPlayers()) do
+                if player and player:IsA("Player") then
+                    TeleportService:Teleport(13775256536, player)
+                end
             end
         end
     end
-end
 
-while true do
-    checkTimer()
-    task.wait(5)
+    while true do
+        checkTimer()
+        task.wait(5)
+    end
 end
